10ebcac783c6ab2b172a701082682d8e8b71e548
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2021-03-14 10:13:17 -0400

    released at 0.1.8

--- xwm.h
+++ xwm.h
@@ -10,18 +10,18 @@ typedef struct {
 
 typedef struct {
     uint32_t request;
-    void (*func)(xcb_generic_event_t * ev);
+    void (*func)(xcb_generic_event_t *ev);
 } handler_func_t;
 
 /* convert keycode to keysym and back */
-static xcb_keycode_t * xcb_get_keycodes(xcb_keysym_t keysym);
+static xcb_keycode_t *xcb_get_keycodes(xcb_keysym_t keysym);
 static xcb_keysym_t    xcb_get_keysym(xcb_keycode_t keycode);
 
 /* user defined command actions */
 static void killclient(char **com);
 static void spawn(char **com);
-static void closewm(char ** com);
-static void fullclient(char ** com);
+static void closewm(char **com);
+static void fullclient(char **com);
 
 /* window behavior */
 static void setFocus(xcb_drawable_t window);
@@ -29,15 +29,15 @@ static void setFocusColor(xcb_drawable_t window, int focus);
 
 /* event hander actions */
 static int eventHandler(void);
-static void handleMotionNotify(xcb_generic_event_t * ev);
-static void handleEnterNotify(xcb_generic_event_t * ev);
-static void handleDestroyNotify(xcb_generic_event_t * ev);
-static void handleButtonPress(xcb_generic_event_t * ev);
-static void handleButtonRelease(xcb_generic_event_t * ev);
-static void handleKeyPress(xcb_generic_event_t * ev);
-static void handleMapRequest(xcb_generic_event_t * ev);
-static void handleFocusIn(xcb_generic_event_t * ev);
-static void handleFocusOut(xcb_generic_event_t * ev);
+static void handleMotionNotify(xcb_generic_event_t *ev);
+static void handleEnterNotify(xcb_generic_event_t *ev);
+static void handleDestroyNotify(xcb_generic_event_t *ev);
+static void handleButtonPress(xcb_generic_event_t *ev);
+static void handleButtonRelease(xcb_generic_event_t *ev);
+static void handleKeyPress(xcb_generic_event_t *ev);
+static void handleMapRequest(xcb_generic_event_t *ev);
+static void handleFocusIn(xcb_generic_event_t *ev);
+static void handleFocusOut(xcb_generic_event_t *ev);
 static handler_func_t handler_funs[] = {
     { XCB_MOTION_NOTIFY,  handleMotionNotify },
     { XCB_ENTER_NOTIFY,   handleEnterNotify },
@@ -55,5 +55,5 @@ static handler_func_t handler_funs[] = {
 static void setup(void);
 
 /* error handling & misc. */
-static int die(char * errstr);
-static int strcmp_c(char * str1, char * str2);
+static int die(char *errstr);
+static int strcmp_c(char *str1, char *str2);
